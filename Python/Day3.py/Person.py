9d4ab8d9aae320181ef888c73f70e50f5caa1f07
PunitCodeX <punitdhankhar21@gmail.com>
2025-11-13 20:44:47 +0530

    Person.py

---
+++ Python/Day3.py/Person.py
@@ -0,0 +1,10 @@
+#Made By Punit
+
+=================== x x x x x x x x ===================
+
+def greet(name):
+    return f"My name is {name}"
+
+=================== x x x x x x x x ===================
+
+#Made By Punit
